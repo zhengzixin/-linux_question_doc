ac565b172becfb9a71eb16f9fb77557342e30e1f
zhengzixin <ait_zhengchunyu@163.com>
2020-05-22 15:40:12 +0800

    modification conf.py del pdflatex

--- conf.py
+++ conf.py
@@ -136,47 +136,6 @@ html_sidebars = {
 # 
 # html_last_updated_fmt = ''
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-    
-    'inputenc': '',
-    'utf8extra': '',
-    'classoptions': ',openany,oneside',
-    'preamble': r'''
-
-\usepackage{fontspec}
-\setmonofont{DejaVu Sans Mono}
-\usepackage{xeCJK}
-\setCJKmainfont{SimSun}
-\usepackage{silence}
-\WarningsOff*
-''',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'LittlevGL.tex', 'LittlevGL Documentation',
-     'Gabor Kiss-Vamosi', 'manual'),
-]
-
 
 # -- Options for manual page output ---------------------------------------
 
